b6de305505c094fa3499365ce392280d677c8397
Alun Williams <alun.williams@zuto.com>
2018-03-25 20:42:18 +0100

    File clean up

--- Vigenere.Tests/GridLookupTests.cs
+++ Vigenere.Tests/GridLookupTests.cs
@@ -13,11 +13,11 @@ namespace Tests
             this.lookup = new GridLookup();
         }
 
-        [TestCase('A', 'A', 0)]
-        [TestCase('B', 'B', 0)]
+        [TestCase('A', 'A')]
+        [TestCase('B', 'B')]
         public void Should_ReturnSameChar_When_Lookup(char input, char expected)
         {
-            char result = lookup.Lookup(input);
+            char result = lookup.Lookup(input, 0);
 
             Assert.That(input, Is.EqualTo(result));
         }
@@ -27,7 +27,7 @@ namespace Tests
         [TestCase('A', 1, 'B')]
         public void Should_ReturnCorrectChar_When_LookupWithIndex(char input, char expected)
         {
-            char result = lookup.Lookup(input);
+            char result = lookup.Lookup(input, 0);
 
             Assert.That(input, Is.EqualTo(result));
         }
