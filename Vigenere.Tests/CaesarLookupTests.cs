736122a51fdabd2d89eb3bf4ecad53fa71c138d5
Alun Williams <alun.williams@zuto.com>
2018-03-25 20:46:48 +0100

    Removed tests

--- Vigenere.Tests/CaesarLookupTests.cs
+++ Vigenere.Tests/CaesarLookupTests.cs
@@ -13,16 +13,6 @@ namespace Tests
             this.caesar = new CaesarLookup();
         }
 
-        [TestCase('A', 'A')]
-        [TestCase('B', 'B')]
-        public void Should_ReturnSameChar_When_Lookup(char input, char expected)
-        {
-            char result = caesar.Lookup(input, 0);
-
-            Assert.That(input, Is.EqualTo(result));
-        }
-
-
         [TestCase('A', 0, 'A')]
         [TestCase('A', 1, 'B')]
         public void Should_ReturnCorrectChar_When_LookupWithIndex(char input, char expected)
