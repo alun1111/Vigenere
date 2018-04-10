f8d3528c6418b06c9eae601c6933f3b7cfdae372
Alun Williams <alun.williams@zuto.com>
2018-04-10 21:32:14 +0100

    Added main program ui

--- Vigenere/Program.cs
+++ Vigenere/Program.cs
@@ -6,7 +6,29 @@ namespace Vigenere
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            VigenereEncoder v = new VigenereEncoder();
+
+            Console.WriteLine("Encode (e) or Decode(d)?");
+            ConsoleKey key = Console.ReadKey().Key;
+
+            if(key != ConsoleKey.E && key != ConsoleKey.D){
+                throw new ApplicationException("Unknown choice");
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("Enter keyword and press enter (caps only):");
+            string keyword = Console.ReadLine();
+            Console.WriteLine("Enter message and press enter (caps only):");
+            string message = Console.ReadLine();
+            
+            if(key.Equals(ConsoleKey.E))
+            {
+                Console.WriteLine(v.Encrypt(message, keyword));
+            } 
+            else if(key.Equals(ConsoleKey.D))
+            {
+                Console.WriteLine(v.Decrypt(message, keyword));
+            }
         }
     }
 }
